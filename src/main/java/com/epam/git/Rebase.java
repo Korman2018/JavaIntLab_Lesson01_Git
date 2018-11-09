7770b11eea22d2f95fef569b28659ce05bfdf8ae
Konstantin_Korostelev <Konstantin_Korostelev@epam.com>
2018-11-09 16:03:34 +0400

    rebase2

--- src/main/java/com/epam/git/Rebase.java
+++ src/main/java/com/epam/git/Rebase.java
@@ -2,6 +2,6 @@ package main.java.com.epam.git;
 
 public class Rebase {
     public static void main(String[] args) {
-        System.out.println("class for rebase");
+        System.out.println("class for rebase2");
     }
 }
