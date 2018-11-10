a675b81c54359f2989f3a4368bb5b3587a9844d6
Konstantin Korostelev <koriman2016@gmail.com>
2018-11-10 23:50:14 +0400

    test amend

--- src/main/java/com/epam/git/Five.java
+++ src/main/java/com/epam/git/Five.java
@@ -4,5 +4,6 @@ public class Five {
     public static void main(String[] args) {
         System.out.println("This is class Five");
         System.out.println("do some changes");
+        System.out.println("test amend2");
     }
 }
--- src/main/java/com/epam/git/Two.java
+++ src/main/java/com/epam/git/Two.java
@@ -3,5 +3,6 @@ package main.java.com.epam.git;
 public class Two {
     public static void main(String[] args) {
         System.out.println("This is class Two");
+        System.out.println("checkout");
     }
 }
