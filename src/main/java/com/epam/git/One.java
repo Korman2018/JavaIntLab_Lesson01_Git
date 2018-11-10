ab149b2d3bac7535e8611775cd683699830ba642
Konstantin Korostelev <koriman2016@gmail.com>
2018-11-10 12:40:07 +0400

    test pull merge

--- src/main/java/com/epam/git/One.java
+++ src/main/java/com/epam/git/One.java
@@ -3,5 +3,6 @@ package main.java.com.epam.git;
 public class One {
     public static void main(String[] args) {
         System.out.println("This is class One");
+        System.out.println("test pull merge");
     }
 }
