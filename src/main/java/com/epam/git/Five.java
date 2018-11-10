37db0b537baabce82a34dd5a77207d1fc8b846bc
Konstantin Korostelev <koriman2016@gmail.com>
2018-11-10 12:02:13 +0400

    do some changes in class Five

--- src/main/java/com/epam/git/Five.java
+++ src/main/java/com/epam/git/Five.java
@@ -3,5 +3,6 @@ package main.java.com.epam.git;
 public class Five {
     public static void main(String[] args) {
         System.out.println("This is class Five");
+        System.out.println("do some changes");
     }
 }
