e8f096ec4410915ebdbca448b916d315f84b7dc3
Konstantin Korostelev <koriman2016@gmail.com>
2018-11-10 12:04:44 +0400

    do some changes2 in class Four

--- src/main/java/com/epam/git/Four.java
+++ src/main/java/com/epam/git/Four.java
@@ -4,5 +4,6 @@ public class Four {
     public static void main(String[] args) {
         System.out.println("This is class Four");
         System.out.println("some changes");
+        System.out.println("some changes2");
     }
 }
