40fb5a1d781119998a1adf09fde32e79d0dfbf1b
Konstantin_Korostelev <Konstantin_Korostelev@epam.com>
2018-11-12 10:52:04 +0400

    test revert2

--- src/main/java/com/epam/git/One.java
+++ src/main/java/com/epam/git/One.java
@@ -7,5 +7,6 @@ public class One {
         System.out.println("test pull merge");
         System.out.println("test cherry pick");
         System.out.println("Test revert1");
+        System.out.println("Test revert2");
     }
 }
